6ca63a76930aa333d8d1002fb04b713620526e35
adrianopiacentini <adrianopiacentini03@gmail.com>
2024-11-09 15:26:50 +0100

    Esercizio recuperato

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const switchBtn = document.getElementById('switch');
+const lampImg = document.getElementById('lamp');
+
+switchBtn.addEventListener('click', function() {
+    lampImg.src = './img/yellow_lamp.png';
+
+});
